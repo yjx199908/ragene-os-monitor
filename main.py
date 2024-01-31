755df240438f4ed20663ef28db6ddb422ed0175c
yjx199908 <1399590856@qq.com>
2024-01-31 16:00:00 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import psutil
 import time
 from email_reporter import EmailReporter
+import os
+
+env = os.environ.get("ENV")
+receivers = os.environ.get("RECEIVERS").split(",")
 
 # 监控阈值
 THRESHOLD_CPU_PERCENT = 70
@@ -8,10 +12,10 @@ THRESHOLD_MEMORY_PERCENT = 70
 THRESHOLD_DISK_PERCENT = 70
 
 def send_email(subject, content):
-    receiver = "1399590856@qq.com"
-    reporter = EmailReporter(receiver)
-    reporter.fill_message(subject, content)
-    reporter.send()
+    for receiver in receivers:
+        reporter = EmailReporter(receiver)
+        reporter.fill_message(subject, content)
+        reporter.send()
 
 def check_system_usage():
     cpu_percent = psutil.cpu_percent(interval=1)
@@ -19,15 +23,15 @@ def check_system_usage():
     disk_info = psutil.disk_usage('/')
     if cpu_percent > THRESHOLD_CPU_PERCENT:
         warning_msg = f"警告: CPU使用率超过{THRESHOLD_CPU_PERCENT}%，当前使用率为{cpu_percent}%"
-        send_email("PROD环境系统资源警告", warning_msg)
+        send_email(f"{env}环境系统资源警告", warning_msg)
 
     if memory_info.percent > THRESHOLD_MEMORY_PERCENT:
         warning_msg = f"警告: 内存使用率超过{THRESHOLD_MEMORY_PERCENT}%，当前使用率为{memory_info.percent}%"
-        send_email("PROD环境系统资源警告", warning_msg)
+        send_email(f"{env}环境系统资源警告", warning_msg)
 
     if disk_info.percent > THRESHOLD_DISK_PERCENT:
         warning_msg = f"警告: 磁盘空间使用率超过{THRESHOLD_DISK_PERCENT}%，当前使用率为{disk_info.percent}%"
-        send_email("PROD环境系统资源警告", warning_msg)
+        send_email(f"{env}环境系统资源警告", warning_msg)
 
 if __name__ == "__main__":
     while True:
